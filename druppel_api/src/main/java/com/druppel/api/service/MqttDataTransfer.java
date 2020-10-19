a2378e47c6c8bcfef62d73187b9b92f9c84d986c
nickkersten <cincle@gmail.com>
2020-10-19 18:44:48 +0200

    First implementation of MQTT parser. Basic functionality done; Todo: Class description, method descriptions, Model object creation from parse results; need object model implementation.

--- druppel_api/src/main/java/com/druppel/api/service/MqttDataTransfer.java
+++ druppel_api/src/main/java/com/druppel/api/service/MqttDataTransfer.java
@@ -1,4 +1,12 @@
 package com.druppel.api.service;
 
+import com.druppel.api.model.Plant;
+
 public class MqttDataTransfer {
+    MqttParser mqttParser = new MqttParser();
+    Plant plant;
+
+    public void saveMeasurement(String mqttHeader, String mqttPayload) {
+        plant = mqttParser.parse(mqttHeader, mqttPayload);
+    }
 }
