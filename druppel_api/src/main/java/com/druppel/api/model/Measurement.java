672922b03bc917bb3cd95d2be2cfaf849b5ba8dc
Iness <437952@student.fontys.nl>
2020-10-19 20:28:25 +0200

    eerste opzet model

--- druppel_api/src/main/java/com/druppel/api/model/Measurement.java
+++ druppel_api/src/main/java/com/druppel/api/model/Measurement.java
@@ -1,4 +1,23 @@
 package com.druppel.api.model;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "measurement" )
+
 public class Measurement {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(name = "date")
+    private Date date;
+
+    @Column(name = "type")
+    private String type;
+
+    @Column(name = "value")
+    private Float value;
 }
+
