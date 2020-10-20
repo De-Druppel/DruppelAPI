0b743baf190798ba61f1a1fa1c3d669fca74967f
Tygovanommen <tygovanommen@live.nl>
2020-10-20 13:24:56 +0200

    Return Json in GET request

--- druppel_api/src/main/java/com/druppel/api/service/RestDataTransfer.java
+++ druppel_api/src/main/java/com/druppel/api/service/RestDataTransfer.java
@@ -1,10 +1,27 @@
 package com.druppel.api.service;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class RestDataTransfer {
     public RestDataTransfer() {
     }
 
     public String get(String espId, String timeframe) {
-        return "Result";
+
+        // JSON OBJECT EXAMPLE
+        JSONObject result = new JSONObject();
+        try {
+            JSONObject data = new JSONObject();
+            data.put("Temprature 1", "23");
+            data.put("Temprature 2", "54");
+            data.put("Temprature 3", "12");
+
+            result.put("Data", data);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return result.toString();
     }
 }
