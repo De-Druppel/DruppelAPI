c5dea6a1dee2efa35c1a5b85e16ab81fbf27f069
nickkersten <cincle@gmail.com>
2020-11-01 15:10:35 +0100

    - Created MQTT client beans configuration. - Implemented Spring IOC for MQTT client.
    - Created MQTT publisher beans configuration.
    - Implemented Spring IoC for MQTT publisesher.

---
+++ src/main/java/com/druppel/api/controller/MqttClientConfiguration.java
@@ -0,0 +1,86 @@
+package com.druppel.api.controller;
+
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.core.MessageProducer;
+import org.springframework.integration.mqtt.core.DefaultMqttPahoClientFactory;
+import org.springframework.integration.mqtt.core.MqttPahoClientFactory;
+import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter;
+import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter;
+import org.springframework.messaging.MessageChannel;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * This class is responsible for setting up spring IoC container beans regarding MQTT client configuration.
+ */
+
+@Configuration
+public class MqttClientConfiguration {
+    private final ApplicationContext context;
+
+    public MqttClientConfiguration(ApplicationContext context) {
+        this.context = context;
+    }
+
+    /**
+     * @return subscribable message channel for single subscriber.
+     */
+    @Bean
+    public DirectChannel mqttInputChannel() {
+        return new DirectChannel() {
+        };
+    }
+
+    /**
+     * @return MQTT subscriber details properties from properties file.
+     */
+    @Bean
+    public Properties properties() {
+        Properties properties = new Properties();
+        try {
+            properties.load(MqttClientConfiguration.class.getClassLoader().getResourceAsStream("mqtt.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return properties;
+    }
+
+    /**
+     * Sets connection options for the Mqtt Client factory.
+     *
+     * @return mqtt client factory.
+     */
+    @Bean
+    public MqttPahoClientFactory mqttClientFactory() {
+        DefaultMqttPahoClientFactory factory = new DefaultMqttPahoClientFactory();
+        MqttConnectOptions options = new MqttConnectOptions();
+        // Set mqtt connection options
+        options.setServerURIs(new String[]{properties().getProperty("SERVERURL")});
+        options.setUserName(properties().getProperty("USERNAME"));
+        options.setPassword(properties().getProperty("PASSWORD").toCharArray());
+        factory.setConnectionOptions(options);
+        return factory;
+    }
+
+    /**
+     * Creates and sets up adapter for Mqtt and Messagechannel endpoints.
+     *
+     * @return inbound message producer.
+     */
+    @Bean
+    public MessageProducer inbound() {
+        //Generate unique clientId.
+        String clientId = properties().getProperty("CLIENT_ID") + System.currentTimeMillis();
+        MqttPahoMessageDrivenChannelAdapter adapter =
+                new MqttPahoMessageDrivenChannelAdapter(clientId, mqttClientFactory(), properties().getProperty("MQTT_TOPIC"));
+        adapter.setCompletionTimeout(5000);
+        adapter.setConverter(new DefaultPahoMessageConverter());
+        adapter.setQos(1);
+        adapter.setOutputChannel(context.getBean("mqttInputChannel", MessageChannel.class));
+        return adapter;
+    }
+}
---
+++ src/main/java/com/druppel/api/controller/MqttClientController.java
@@ -0,0 +1,48 @@
+package com.druppel.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.config.EnableIntegration;
+import org.springframework.integration.endpoint.EventDrivenConsumer;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHandler;
+import org.springframework.messaging.MessagingException;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Thi class is responsible for handling incloming MQTT messages.
+ */
+@Controller
+@EnableIntegration
+public class MqttClientController {
+
+    @Autowired
+    ApplicationContext applicationContext;
+
+    public MqttClientController() {
+    }
+
+    /**
+     * Method that is capable of handling a message.
+     * inputChannel: the channel from which this service activator will consume messages.
+     */
+    @ServiceActivator(inputChannel = "mqttInputChannel")
+    public MessageHandler handler() {
+        return message -> {
+            System.out.println(message.getPayload());
+            System.out.println(message.getHeaders());
+        };
+    }
+
+    @Bean
+    public EventDrivenConsumer eventDrivenConsumer() {
+        DirectChannel mqttInputChannel = applicationContext.getBean("mqttInputChannel", DirectChannel.class);
+        return new EventDrivenConsumer(mqttInputChannel, handler());
+    }
+
+
+
+}
