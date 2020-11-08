f5486066a854c4d72658901e6951ec7ba88d861d
Iness <437952@student.fontys.nl>
2020-11-08 22:23:07 +0100

    Delete unnecessary method

--- src/main/java/com/druppel/api/dal/MeasurementRepo.java
+++ src/main/java/com/druppel/api/dal/MeasurementRepo.java
@@ -11,15 +11,6 @@ import java.util.List;
 @Repository
 public interface MeasurementRepo extends JpaRepository <Measurement, Integer> {
 
-    /**
-    * Returns average of measured values for specific type on specific date for specific microcontroller
-     @param  type String for type of the measured value
-     @param  date Date for date where the values were measured
-     @param  espId int for microcontroller Id
-     @return  Float average of measured values
-     */
-    @Query("SELECT AVG (value) FROM Measurement WHERE type=?1 AND date=?2 AND espId =?3" )
-    public Float averageValue (String type, Date date,int espId);
 
     /**
      * Returns list of average of measured values grouped by day and type on a specific time interval
