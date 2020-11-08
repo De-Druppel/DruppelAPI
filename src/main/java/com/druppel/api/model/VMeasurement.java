647520743daa419e60dd0d6ea06ca3e0d13808b7
Iness <437952@student.fontys.nl>
2020-11-08 22:37:54 +0100

    Create VMeasurement for mapping of the view table

---
+++ src/main/java/com/druppel/api/model/VMeasurement.java
@@ -0,0 +1,29 @@
+package com.druppel.api.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "v_measurements")
+public class VMeasurement {
+
+    // need to implement an Id
+ /*   @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;*/
+
+    @Column(name = "esp_id")
+    private int espId;
+
+    @Column(name = "date_created")
+    private Date date;
+
+    @Column(name = "date_created")
+    private Date date;
+
+    @Column(name = "type")
+    private String type;
+
+    @Column(name = "value")
+    private Float value;
+
+}
